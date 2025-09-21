259d732213beeb6e98b5c1045cad05e2ea6cd5d6
jman1593 <josh@pettits.net>
2025-09-21 16:04:02 -0600

    add more games to testing

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,11 @@
 export default [
+      
+      
+      
+      { href: './html/Spades/index.html', imgSrc: 'Spades.jpeg', name: 'Spades' , 'page': 5, 'id': 745 },
+{ href: './html/Scary Teacher 3D/index.html', imgSrc: 'Scary Teacher 3D.jpeg', name: 'Scary Teacher 3D' , 'page': 5, 'id': 744 },
+{ href: './html/Cryzen.io/index.html', imgSrc: 'Cryzen.io.jpeg', name: 'Cryzen.io' , 'page': 5, 'id': 743 },
+{ href: './html/American Football Game/index.html', imgSrc: 'American Football Game.jpeg', name: 'American Football Game' , 'page': 5, 'id': 738 },
 { href: './html/Happy Wheels/index.html', imgSrc: 'Happy Wheels.jpeg', name: 'Happy Wheels' , 'page': 5, 'id': 396 },
 { href: './html/Fabled Conquerors/index.html', imgSrc: 'Fabled Conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 203 },
 { href: './html/DOOM/index.html', imgSrc: 'DOOM.jpeg', name: 'DOOM' , 'page': 5, 'id': 165 },

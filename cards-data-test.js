56a3736758e32d151455eac207a4c64bb622e072
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-09-21 13:49:27 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,5 +1,4 @@
 export default [
-{ href: './html/Last Breath/index.html', imgSrc: 'Last Breath.jpeg', name: 'Last Breath' , 'page': 5, 'id': 720 },
 { href: './html/Happy Wheels/index.html', imgSrc: 'Happy Wheels.jpeg', name: 'Happy Wheels' , 'page': 5, 'id': 396 },
 { href: './html/Fabled Conquerors/index.html', imgSrc: 'Fabled Conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 203 },
 { href: './html/DOOM/index.html', imgSrc: 'DOOM.jpeg', name: 'DOOM' , 'page': 5, 'id': 165 },

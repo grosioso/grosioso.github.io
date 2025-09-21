c40b818617d21f25efaf854fec4518d80700f81e
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-09-21 00:15:15 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,7 +1,6 @@
 export default [
 { href: './html/Last Breath/index.html', imgSrc: 'Last Breath.jpeg', name: 'Last Breath' , 'page': 5, 'id': 720 },
 { href: './html/Happy Wheels/index.html', imgSrc: 'Happy Wheels.jpeg', name: 'Happy Wheels' , 'page': 5, 'id': 396 },
-{ href: './html/Grow a Garden/index.html', imgSrc: 'Grow a Garden.jpeg', name: 'Grow a Garden' , 'page': 5, 'id': 250 },
 { href: './html/Fabled Conquerors/index.html', imgSrc: 'Fabled Conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 203 },
 { href: './html/DOOM/index.html', imgSrc: 'DOOM.jpeg', name: 'DOOM' , 'page': 5, 'id': 165 },
 { href: './html/City Car Driving Stunt Master/index.html', imgSrc: 'City Car Driving Stunt Master.jpeg', name: 'City Car Driving Stunt Master' , 'page': 5, 'id': 155 },

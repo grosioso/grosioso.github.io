d83746022c41dc1bd6d771c04f1acc8ee95535a4
jman1593 <josh@pettits.net>
2025-09-20 21:24:32 -0600

    add new games

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,3 +1,10 @@
 export default [
-      { href: './html/clash royale/index.html', imgSrc: 'clash royale.jpeg', name: 'Clash Royale (ripoff)' , 'page': 5, 'id': 945 },
+{ href: './html/Steal a Brainrot/index.html', imgSrc: 'Steal a Brainrot.jpeg', name: 'Steal a Brainrot' , 'page': 5, 'id': 731 },
+{ href: './html/Last Breath/index.html', imgSrc: 'Last Breath.jpeg', name: 'Last Breath' , 'page': 5, 'id': 720 },
+{ href: './html/Happy Wheels/index.html', imgSrc: 'Happy Wheels.jpeg', name: 'Happy Wheels' , 'page': 5, 'id': 396 },
+{ href: './html/Grow a Garden/index.html', imgSrc: 'Grow a Garden.jpeg', name: 'Grow a Garden' , 'page': 5, 'id': 250 },
+{ href: './html/Fabled Conquerors/index.html', imgSrc: 'Fabled Conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 203 },
+{ href: './html/DOOM/index.html', imgSrc: 'DOOM.jpeg', name: 'DOOM' , 'page': 5, 'id': 165 },
+{ href: './html/City Car Driving Stunt Master/index.html', imgSrc: 'City Car Driving Stunt Master.jpeg', name: 'City Car Driving Stunt Master' , 'page': 5, 'id': 155 },
+{ href: './html/clash royale/index.html', imgSrc: 'clash royale.jpeg', name: 'Clash Royale (ripoff)' , 'page': 5, 'id': 945 },
 ]

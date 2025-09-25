e9ea18842541150a4cbd7fbea6bcf1ef57ca45fc
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-09-25 07:25:47 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -3,7 +3,6 @@ export default [
 { href: './html/Scary Teacher 3D/index.html', imgSrc: 'Scary Teacher 3D.jpeg', name: 'Scary Teacher 3D' , 'page': 5, 'id': 744 },
 { href: './html/Cryzen.io/index.html', imgSrc: 'Cryzen.io.jpeg', name: 'Cryzen.io' , 'page': 5, 'id': 743 },
 { href: './html/American Football Game/index.html', imgSrc: 'American Football Game.jpeg', name: 'American Football Game' , 'page': 5, 'id': 738 },
-{ href: './html/Happy Wheels/index.html', imgSrc: 'Happy Wheels.jpeg', name: 'Happy Wheels' , 'page': 5, 'id': 396 },
 { href: './html/Fabled Conquerors/index.html', imgSrc: 'Fabled Conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 203 },
 { href: './html/DOOM/index.html', imgSrc: 'DOOM.jpeg', name: 'DOOM' , 'page': 5, 'id': 165 },
 { href: './html/clash royale/index.html', imgSrc: 'clash royale.jpeg', name: 'Clash Royale (ripoff)' , 'page': 5, 'id': 945 },
